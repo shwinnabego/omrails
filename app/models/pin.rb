f94efefab70be5f24066f2dd1b02d55aabafaf02
Ashwinn <ashwinn.krishnaswamy@gmail.com>
2013-04-04 23:27:07 -0400

    ugg

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -6,12 +6,10 @@ class Pin < ActiveRecord::Base
   validates :user_id, presence: true
   validates_attachment :image, presence: true,
   								image_content_type: {content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] }, 
-								image_size:  { less_than: 5.megabytes }  								
+								size:  { less_than: 5.megabytes }  								
   belongs_to :user
   has_attached_file :image, styles: { medium: "320x240>"}
   
-  attr_accessor :image_file_name
-
-
+ 
 
 end
